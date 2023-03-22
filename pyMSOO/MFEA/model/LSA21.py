674a06448dc6197689a3b209f57005b9eb0fc77d
kaito0311 <tanminh8b@gmail.com>
2023-03-22 23:45:09 +0700

    .

--- pyMSOO/MFEA/model/LSA21.py
+++ pyMSOO/MFEA/model/LSA21.py
@@ -7,10 +7,11 @@ import numpy as np
 class model(AbstractModel.model):
 
     class rmp_lsa21: 
-        def __init__(self, default_rmp= 0.5) -> None:
+        def __init__(self, nb_tasks, default_rmp= 0.5) -> None:
             self.default_rmp = default_rmp
             self.C = 0.02 
             self.best_partner = None 
+            self.nb_tasks = nb_tasks
 
 
             self.rmp = np.zeros(shape=(self.nb_tasks, self.nb_tasks)) + self.default_rmp
@@ -18,12 +19,46 @@ class model(AbstractModel.model):
 
             self.s_rmp = np.empty(shape= (self.nb_tasks, self.nb_tasks,0)).tolist()
             self.diff_f_inter_x = np.empty(shape=(self.nb_tasks, self.nb_tasks,0)).tolist() 
+
+        def update(self, *args, **kwargs) -> None:
+            for task in range(self.nb_tasks):
+                maxRmp = 0 
+                self.best_partner[task] = -1 
+                for task2 in range(self.nb_tasks): 
+                    if task2 == task: 
+                        continue 
+                    
+                    good_mean = 0 
+                    if len(self.s_rmp[task][task2]) > 0: 
+                        sum = np.sum(np.array(self.diff_f_inter_x[task][task2])) 
+
+                        w = np.array(self.diff_f_inter_x[task][task2]) / sum 
+
+                        val1 =  np.sum(w * np.array(self.s_rmp[task][task2]) ** 2) 
+                        val2 = np.sum(w * np.array(self.s_rmp[task][task2])) 
+
+                        good_mean = val1 / val2 
+
+                        if (good_mean > self.rmp[task][task2] and good_mean > maxRmp): 
+                            maxRmp = good_mean 
+                            self.best_partner[task] = task2 
+                        
+                    
+                    if good_mean > 0: 
+                        c1 = 1.0 
+                    else: 
+                        c1 = 1.0 - self.C 
+                    self.rmp[task][task2] = c1 * self.rmp[task][task2] + self.C * good_mean 
+                    self.rmp[task][task2] = np.max([0.01, np.min([1, self.rmp[task][task2]])])
+
+            self.s_rmp = np.empty(shape= (self.nb_tasks, self.nb_tasks,0)).tolist()
+            self.diff_f_inter_x = np.empty(shape=(self.nb_tasks, self.nb_tasks,0)).tolist() 
+
         
-
-
             
     def __init__(self, seed= None, percent_print= 2): 
         super().__init__(seed, percent_print) 
+
     
     def compile(self, 
         IndClass: Type[Individual], 
@@ -36,6 +71,7 @@ class model(AbstractModel.model):
         super().compile(IndClass, tasks, crossover, mutation, selection, *args, **kwargs)
         self.search = search 
         self.search.getInforTasks(IndClass, tasks, seed= self.seed) 
+        self.rmp= self.rmp_lsa21(len(tasks))
     
     def fit(self, nb_generations: int, nb_inds_each_task: int, nb_inds_min= None,
             lr= 1,
@@ -91,10 +127,10 @@ class model(AbstractModel.model):
                             eval_k[skf] += 1 
                         else: 
                             rmp = 0 
-                            if self.crossover.best_partner[skf] == partner_task: 
+                            if self.rmp.best_partner[skf] == partner_task: 
                                 rmp =1 
                             else : 
-                                mu_rmp = self.crossover.rmp[skf][partner_task] 
+                                mu_rmp = self.rmp.rmp[skf][partner_task] 
                                 
                                 rmp = np.random.normal(loc= mu_rmp, scale= 0.1 )
                                 while rmp <= 0 or rmp > 1: 
@@ -117,8 +153,8 @@ class model(AbstractModel.model):
                                 if delta == 0: 
                                     offsprings[skf].__addIndividual__(survival) 
                                 elif delta > 0: 
-                                    self.crossover.s_rmp[skf][partner_task].append(rmp) 
-                                    self.crossover.diff_f_inter_x[skf][partner_task].append(delta)
+                                    self.rmp.s_rmp[skf][partner_task].append(rmp) 
+                                    self.rmp.diff_f_inter_x[skf][partner_task].append(delta)
                                     offsprings[skf].__addIndividual__(survival) 
                                 else: 
                                     offsprings[skf].__addIndividual__(ind)
@@ -150,6 +186,7 @@ class model(AbstractModel.model):
 
                 self.selection(population, nb_inds_tasks)
                 # update operators
+                self.rmp.update()
                 self.crossover.update(population = population)
                 self.mutation.update(population = population)
                 self.search.update(population) 
@@ -178,7 +215,7 @@ class model(AbstractModel.model):
                     self.history_cost.append([ind.fcost for ind in population.get_solves()])
                     # self.history_smp.append([M_smp[i].get_smp() for i in range(len(self.tasks))])
 
-                    self.render_process(np.sum(eval_k)/MAXEVALS, ['Pop_size', 'Cost'], [[len(population)], self.history_cost[-1]], use_sys= True, print_format_e= False)
+                    self.render_process(np.sum(eval_k)/MAXEVALS, ['Pop_size', 'Cost'], [[len(population)], self.history_cost[-1]], use_sys= True, print_format_e= True)
 
                     # update mutation
                     self.mutation.update(population = population)
@@ -188,7 +225,7 @@ class model(AbstractModel.model):
             
             
             self.last_pop = population
-            self.render_process(epoch/nb_generations, ['Pop_size', 'Cost'], [[len(population)], self.history_cost[-1]], use_sys= True, print_format_e= False)
+            self.render_process(epoch/nb_generations, ['Pop_size', 'Cost'], [[len(population)], self.history_cost[-1]], use_sys= True, print_format_e= True)
             print()
             # print(p_choose_father)
             print(eval_k)
