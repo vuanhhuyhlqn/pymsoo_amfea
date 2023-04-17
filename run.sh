97237421c96b87963cb3377b3a4988deec18c1c9
Kaito Kuroba <tanminh8b@gmail.com>
2023-04-17 23:25:09 +0700

    done docker

--- run.sh
+++ run.sh
@@ -2,8 +2,10 @@ cp pyMSOO/RunModel/Run_SM_MFEA_SBX.py run.py
 python run.py --lr 0.1 \
               --nb_generations 100 \
               --name_benchmark CEC17 \
-              --ls_id_run '1-1-1' \
               --number_tasks 10 \
+              --ls_id_run '1-1-1' \
+              --save_path './RESULTS/' \
+
 
                   
               
