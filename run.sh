7a6cc51dc3e8eaf089cdc41d2645f8298f2edf8b
Kaito Kuroba <tanminh8b@gmail.com>
2023-04-26 01:01:32 +0700

    fix save model

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
-MODEL=MFEA_DaS
+MODEL=MaTGAwDaS
 cp pyMSOO/RunModel/$MODEL/run.py run.py
 cp pyMSOO/RunModel/$MODEL/cfg.yaml cfg.yaml
 
-python run.py --nb_generations 100 \
-              --name_benchmark CEC17 \
-              --number_tasks 10 \
+python run.py --nb_generations 10 \
+              --name_benchmark WCCI22 \
+              --number_tasks 50 \
               --ls_id_run '1-1-1' \
               --save_path './RESULTS/' \
